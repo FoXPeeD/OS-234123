fab83e1b3cb77d08e24b0d8f4870efcace1c73be
FoXPeeD <topitzik@gmail.com>
2016-04-05 18:04:08 +0300

    sanity test

---
+++ hw1/tests/blocker.h
@@ -0,0 +1,96 @@
+
+// #BENITZIK: All of our code wrappers. UserSpace!!
+
+#include <errno.h>
+
+int block_program(const char *name, unsigned int name_len) 
+{
+	unsigned int res;
+	__asm__
+	(
+		"int $0x80;"
+		: "=a" (res)
+		: "0" (243) ,"b" (name) ,"c" (name_len)
+		: "memory"
+	);
+	if (res>= (unsigned long)(-125))
+	{
+		errno = -res;
+		res = -1;
+	}
+	return (int) res;
+}
+
+int unblock_program (const char *name, unsigned int
+name_len) 
+{
+	unsigned int res;
+	__asm__
+	(
+		"int $0x80;"
+		: "=a" (res)
+		: "0" (244) ,"b" (name) ,"c" (name_len)
+		: "memory"
+	);
+	if (res>= (unsigned long)(-125))
+	{
+		errno = -res;
+		res = -1;
+	}
+	return (int) res;
+}
+
+int is_program_blocked(const char *name, unsigned int name_len)
+{
+	unsigned int res;
+	__asm__
+	(
+		"int $0x80;"
+		: "=a" (res)
+		: "0" (245) ,"b" (name) ,"c" (name_len)
+		: "memory"
+	);
+	if (res>= (unsigned long)(-125))
+	{
+		errno = -res;
+		res = -1;
+	}
+	return (int) res;
+}
+
+int get_blocked_count (void)
+{
+	unsigned int res;
+	__asm__
+	(
+		"int $0x80;"
+		: "=a" (res)
+		: "0" (246)
+		: "memory"
+	);
+	if (res>= (unsigned long)(-125))
+	{
+		errno = -res;
+		res = -1;
+	}
+	return (int) res;
+}
+
+int get_forbidden_tries (int pid, char log[][256], unsigned int n)
+{
+	unsigned int res;
+	__asm__
+	(
+		"int $0x80;"
+		: "=a" (res)
+		: "0" (247) ,"b" (pid) ,"c" (log), "d" (n)
+		: "memory"
+	);
+	if (res>= (unsigned long)(-125))
+	{
+		errno = -res;
+		res = -1;
+	}
+	return (int) res;
+}
+
--- hw1/tests/sanityTest.c
+++ hw1/tests/sanityTest.c
@@ -1,19 +1,30 @@
-#include "blocker.h"
-#include <stdlib.h>
+/*
+ * basic_test.c
+ *
+ *  Created on: 5 ????? 2016
+ *      Author: user
+ */
 
+
+#include "blocker.h"
 
 int main(){
+
 	const char* name = "hello\0";
 	unsigned int name_len = 6;
 	int pid = 50;
 	unsigned int n = 13;
-	char* ret_log = malloc(*size_of(char)*256*n);
-	
+	char ret_log[n][256];
+
+
 	block_program(name, name_len);
 	unblock_program(name, name_len);
 	is_program_blocked(name, name_len);
 	get_blocked_count();
 	get_forbidden_tries(pid,ret_log,n);
+
+
+
 
 	return 0;
 }
