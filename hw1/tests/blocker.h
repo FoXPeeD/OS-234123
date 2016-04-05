85a42a37a962354d10d624f1904c689804943786
FoXPeeD <topitzik@gmail.com>
2016-04-05 19:22:57 +0300

    senity test

--- hw1/tests/blocker.h
+++ hw1/tests/blocker.h
@@ -13,6 +13,7 @@ int block_program(const char *name, unsigned int name_len)
 		: "0" (243) ,"b" (name) ,"c" (name_len)
 		: "memory"
 	);
+	printf("[res= %d]",res);
 	if (res>= (unsigned long)(-125))
 	{
 		errno = -res;
@@ -32,6 +33,7 @@ name_len)
 		: "0" (244) ,"b" (name) ,"c" (name_len)
 		: "memory"
 	);
+	printf("[res= %d]",res);
 	if (res>= (unsigned long)(-125))
 	{
 		errno = -res;
@@ -50,6 +52,7 @@ int is_program_blocked(const char *name, unsigned int name_len)
 		: "0" (245) ,"b" (name) ,"c" (name_len)
 		: "memory"
 	);
+	printf("[res= %d]",res);
 	if (res>= (unsigned long)(-125))
 	{
 		errno = -res;
@@ -68,6 +71,7 @@ int get_blocked_count (void)
 		: "0" (246)
 		: "memory"
 	);
+	printf("[res= %d]",res);
 	if (res>= (unsigned long)(-125))
 	{
 		errno = -res;
@@ -86,6 +90,7 @@ int get_forbidden_tries (int pid, char log[][256], unsigned int n)
 		: "0" (247) ,"b" (pid) ,"c" (log), "d" (n)
 		: "memory"
 	);
+	printf("[res= %d]",res);
 	if (res>= (unsigned long)(-125))
 	{
 		errno = -res;
