b42734e20cf612e2bc57b9462b0f9418393536d6
itzik <topitzik@gmail.com>
2016-06-22 02:25:26 +0300

    all function written, not checked

--- hw4/sramdom.c
+++ hw4/sramdom.c
@@ -14,40 +14,56 @@
 #define CHUNK_SIZE 64
 #define POOL_SIZE 512
 #define MIN_ENTROPY_TO_READ 8
+#define READ_CHUNK 20
+
+
+#DEFINE RNDGETENTCNT _IOR(SRANDOM_MAJOR,0,int*)
+#DEFINE RNDCLEARPOOL _IO(SRANDOM_MAJOR,1)
+#DEFINE RNDADDENTROPY _IOW(SRANDOM_MAJOR,2,struct rand_pool_info*)
 
 char pooldata[POOL_SIZE];
 unsigned int entropy_count;
 DECLARE_WAIT_QUEUE_HEAD(my_waitqueue);
 
-int RNDGETENTCNT (int *p)
+int srandom_get_count(int *p)
 {
-	if( !access_ok(VERIFY_WRITE,p,sizeof(int)) ) return -EFAULT;
-	if ( !copy_to_user(p,&entropy_count),sizeof(int) ) return -EFAULT;
+	if( !access_ok(VERIFY_WRITE,p,sizeof(int)) ) 
+		return -EFAULT;
+	if ( !copy_to_user(p,&entropy_count),sizeof(int) ) 
+		return -EFAULT;
 	return 0;
 }
 
-int RNDCLEARPOOL (void)
+int srandom_clear_pool(void)
 {
-	if(!capable(CAP_SYS_ADMIN)) return -EPERM;
+	if(!capable(CAP_SYS_ADMIN)) 
+		return -EPERM;
 	entropy_count = 0;
 	return 0;
 }
 
 
-int RNDADDENTROPY (struct rand_pool_info *p)
+int srandom_add_antropy (struct rand_pool_info *p)
 {
-	if(!capable(CAP_SYS_ADMIN)) return -EPERM;
+	if(!capable(CAP_SYS_ADMIN)) 
+		return -EPERM;
 	
 	char *my_p = kmalloc(sizeof(rand_pool_info), GFP_KERNEL);
-	if( !my_p ) return -NOMEM;
-	if( !copy_from_user(my_p,p,sizeof(rand_pool_info)) )  return -EFAULT;
-	if(my_p->buf_size==0) return -EFAULT;
+	if( !my_p ) 
+		return -NOMEM;
+	if( !copy_from_user(my_p,p,sizeof(rand_pool_info)) )  
+		return -EFAULT;
+	if(my_p->buf_size==0) 
+		return -EFAULT;
 	
-	if(my_p->entropy_count < 0) return -EINVAL;
+	if(my_p->entropy_count < 0) 
+		return -EINVAL;
 	
 	char *my_buf = kmalloc(sizeof(rand_pool_info)+my_p->buf_size, GFP_KERNEL);
-	if( !my_buf ) return -NOMEM;
-	if( !copy_from_user(my_buf,p,sizeof(rand_pool_info)+my_p->buf_size) )  return -EFAULT;
+	if( !my_buf ) 
+		return -NOMEM;
+	if( !copy_from_user(my_buf,p,sizeof(rand_pool_info)+my_p->buf_size) )  
+		return -EFAULT;
 
 	int count = my_p->buf_size;
 	int index = 0;
@@ -59,7 +75,8 @@ int RNDADDENTROPY (struct rand_pool_info *p)
 	mix((my_buf->buf)[index],count,pooldata);
 
 	entropy_count += my_p->entropy_count;
-	if (entropy_count > MAX_ENTROPY) entropy_count = MAX_ENTROPY;
+	if (entropy_count > MAX_ENTROPY) 
+		entropy_count = MAX_ENTROPY;
 	kfree(my_buf);
 	kfree(my_buf);
 	
@@ -87,21 +104,62 @@ static int srandom_release (struct inode *inode, struct file *file)
 static ssize_t srandom_read (struct file *file, char *buf,
 		size_t count, loff_t *ppos)
 {
-	struct simple_data *data = file->private_data;
+	if(count == 0) 
+		return 0;
+	while(MIN_ENTROPY_TO_READ <= entropy_count)
+	{
+		wait_event_interruptible(&my_waitqueue, count >= MIN_ENTROPY_TO_READ);
+		if (signal_pending(current)) 
+			return -ERESTARTSYS;
+	}
+	int E = entropy_count/MIN_ENTROPY_TO_READ;
+	int n = 0;
+	if (count > E) 
+		n = E;
+	else 
+		n = count;
+	
+	entropy_count = entropy_count - (n * MIN_ENTROPY_TO_READ);
+	if ( entropy_count < 0 ) 
+		entropy_count = 0;
 
-	/* XXX implement your read() method */
+	char *my_buf = kmalloc(count, GFP_KERNEL);
+	if( !my_buf ) 
+		return -NOMEM;
+	
+	int left = n;
+	int index = 0;
+	
+	while(left > READ_CHUNK)
+	{
+		hash_pool(pooldata, &(my_buf[index]));
+		mix (&(my_buf[index]), READ_CHUNK, pooldata);
+		
+		left -= READ_CHUNK;
+		index += READ_CHUNK;
+	}
+	char tmp[READ_CHUNK] = {0};
+	hash_pool(pooldata, tmp);
+	mix (tmp, READ_CHUNK, pooldata);
+	memcpy(&(my_buf[index]),tmp,left);
+	
+	if( !copy_from_user(my_buf,buf,n) )  
+		return -EFAULT;
 
-	return 0;
+	return n;
 }
 
 
 static ssize_t srandom_write (struct file *file, const char *buf,
 		size_t count, loff_t *ppos)
 {
-	if(count==0) return -EFAULT;
+	if(count==0) 
+		return -EFAULT;
 	char *my_buf = kmalloc(count, GFP_KERNEL);
-	if( !my_buf ) return -NOMEM;
-	if( !copy_from_user(my_buf,buf,count) )  return -EFAULT;
+	if( !my_buf ) 
+		return -NOMEM;
+	if( !copy_from_user(my_buf,buf,count) )  
+		return -EFAULT;
 	
 	int index = 0;
 	while(count > CHUNK_SIZE){
@@ -120,9 +178,12 @@ static ssize_t srandom_write (struct file *file, const char *buf,
 static int srandom_ioctl (struct inode *inode, struct file *file,
 		unsigned int cmd, unsigned long arg)
 {
-	struct simple_data *data = file->private_data;
-
-	/* XXX implement your ioctl() method */
+	if ( cmd == RNDGETENTCNT ) 
+		return srandom_get_count(arg);
+	if ( cmd == RNDCLEARPOOL ) 
+		return srandom_clear_pool();
+	if ( cmd == RNDADDENTROPY ) 
+		return srandom_add_antropy(arg);
 
 	return -ENOTTY;
 }
@@ -133,7 +194,7 @@ static struct file_operations srandom_fops = {
 	release: srandom_release,
 	read: srandom_read,
 	write: srandom_write,
-	llseek: no_llseek,
+	llseek: NULL,
 	ioctl: srandom_ioctl,
 };
 
@@ -143,7 +204,7 @@ static int __init init_srandom (void)
 
 	/* XXX initialize global variables */
 
-	retval = register_chrdev (SIMPLE_MAJOR, "srandom", &srandom_fops);
+	retval = register_chrdev (SRANDOM_MAJOR, "srandom", &srandom_fops);
 	if (retval < 0)
 		return retval;
 	
@@ -158,7 +219,7 @@ static int __init init_srandom (void)
 
 static void __exit cleanup_srandom (void)
 {
-	unregister_chrdev (SIMPLE_MAJOR, "srandom");
+	unregister_chrdev (SRANDOM_MAJOR, "srandom");
 
 	/* XXX cleanup, release memory, etc. */
 }
