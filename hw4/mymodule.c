3ec55944d8eb58427736c41077a93b254ee5e283
bocaben <bocaben@gmail.com>
2016-06-22 09:36:43 +0300

    cannot compile

--- hw4/mymodule.c
+++ hw4/mymodule.c
@@ -58,37 +58,37 @@ int srandom_clear_pool(void)
 }
 
 
-int srandom_add_entropy (struct rand_pool_info *p)
+int srandom_add_entropy(struct rand_pool_info *p)
 {
 	printk("In add_entropy\n");
-	if(!capable(CAP_SYS_ADMIN)) 
+	if (!capable(CAP_SYS_ADMIN)) 
 		return -EPERM;
 	
 	struct rand_pool_info *my_p = kmalloc(sizeof(struct rand_pool_info), GFP_KERNEL);
-	if( !my_p ) 
+	if (!my_p) 
 		return -ENOMEM;
-	if(0 != copy_from_user(my_p,p,sizeof(struct rand_pool_info)) )  
+	if (0 != copy_from_user(my_p,p,sizeof(struct rand_pool_info)) )  
 		return -EFAULT;
-	if(my_p->buf_size==0) 
+	if (my_p->buf_size==0) 
 		return -EFAULT;
 	
-	if(my_p->entropy_count < 0) 
+	if (my_p->entropy_count < 0) 
 		return -EINVAL;
 	
 	struct rand_pool_info *my_buf = kmalloc(sizeof(struct rand_pool_info)+my_p->buf_size, GFP_KERNEL);
-	if( !my_buf ) 
+	if (!my_buf ) 
 		return -ENOMEM;
-	if(0 != copy_from_user(my_buf, p, sizeof(struct rand_pool_info) + my_p->buf_size) )  
+	if (0 != copy_from_user(my_buf, p, sizeof(struct rand_pool_info) + my_p->buf_size) )  
 		return -EFAULT;
 
 	int count = my_p->buf_size;
 	int index = 0;
-	while(count > CHUNK_SIZE){
-		mix( &((my_buf->buf)[index]) ,CHUNK_SIZE, pooldata);
+	while (count > CHUNK_SIZE){
+		mix (&((my_buf->buf)[index]), CHUNK_SIZE, pooldata);
 		index += CHUNK_SIZE;
 		count -= CHUNK_SIZE;
 	}
-	mix( &((my_buf->buf)[index]), count, pooldata);
+	mix (&((my_buf->buf)[index]), count, pooldata);
 
 	entropy_count += my_p->entropy_count;
 	if (entropy_count > MAX_ENTROPY) 
@@ -96,10 +96,9 @@ int srandom_add_entropy (struct rand_pool_info *p)
 	kfree(my_buf);
 	kfree(my_buf);
 	
-	if( count >= MIN_ENTROPY_TO_READ )
+	if (count >= MIN_ENTROPY_TO_READ)
 		wake_up_interruptible(&my_waitqueue);
 
-	printk("Almost done adding: %d\n", count);
 	return 0;
 	
 }
@@ -109,69 +108,74 @@ int srandom_add_entropy (struct rand_pool_info *p)
 
 /* XXX put your global variables here */
 
-static int srandom_open (struct inode *inode, struct file *file)
+static int srandom_open(struct inode *inode, struct file *file)
 {
 	printk("In OPEN\n");
 	return 0;
 }
 
-static int srandom_release (struct inode *inode, struct file *file)
+static int srandom_release(struct inode *inode, struct file *file)
 {
 	printk("RELEASE\n");
 	return 0;
 }
 
-//TODO:
-static ssize_t srandom_read (struct file *file, char *buf,
-		size_t count, loff_t *ppos)
+static ssize_t srandom_read(struct file *file, char *buf,
+	size_t count, loff_t *ppos)
 {
-	if(count == 0) 
+	printk("In read\n");
+	int n;
+
+	if (count == 0) 
 		return 0;
-	while(MIN_ENTROPY_TO_READ <= entropy_count)
+	while (entropy_count < MIN_ENTROPY_TO_READ)
 	{
-		wait_event_interruptible(my_waitqueue, count >= MIN_ENTROPY_TO_READ);
+		wait_event_interruptible(my_waitqueue, entropy_count >= MIN_ENTROPY_TO_READ);
 		if (signal_pending(current)) 
 			return -ERESTARTSYS;
 	}
-	int E = entropy_count/MIN_ENTROPY_TO_READ;
-	int n = 0;
+	int E = entropy_count / MIN_ENTROPY_TO_READ;
+	
 	if (count > E) 
 		n = E;
-	else 
+	else
 		n = count;
 	
-	entropy_count = entropy_count - (n * MIN_ENTROPY_TO_READ);
-	if ( entropy_count < 0 ) 
+	entropy_count -= MIN_ENTROPY_TO_READ * n;
+	if (entropy_count < 0)
 		entropy_count = 0;
 
 	char *my_buf = kmalloc(count, GFP_KERNEL);
-	if( !my_buf ) 
+	if (!my_buf) 
 		return -ENOMEM;
 	
 	int left = n;
 	int index = 0;
 	
-	while(left > READ_CHUNK)
+	printk("About to copy %d bytes\n", left);
+	while (left > READ_CHUNK)
 	{
 		hash_pool(pooldata, &(my_buf[index]));
 		mix (&(my_buf[index]), READ_CHUNK, pooldata);
 		
 		left -= READ_CHUNK;
 		index += READ_CHUNK;
+		printk("my_buf is now %.*s\n", index+1, my_buf);
 	}
 	char tmp[READ_CHUNK] = {0};
 	hash_pool(pooldata, tmp);
 	mix (tmp, READ_CHUNK, pooldata);
-	memcpy(&(my_buf[index]),tmp,left);
+	memcpy(&(my_buf[index]), tmp, left);
 	
-	if(0 != copy_from_user(my_buf,buf,n) )  
+	if (0 != copy_from_user(buf,my_buf,n))
 		return -EFAULT;
 
+	kfree(my_buf);
 	return n;
 }
 
 
-static ssize_t srandom_write (struct file *file, const char *buf,
+static ssize_t srandom_write(struct file *file, const char *buf,
 		size_t count, loff_t *ppos)
 {
 	if(count==0) 
@@ -196,7 +200,7 @@ static ssize_t srandom_write (struct file *file, const char *buf,
 }
 
 //TODO:
-static int srandom_ioctl (struct inode *inode, struct file *file,
+static int srandom_ioctl(struct inode *inode, struct file *file,
 		unsigned int cmd, unsigned long arg)
 {
 	printk("In ioctl\n");
@@ -239,7 +243,7 @@ static int __init init_srandom (void)
 	return 0;
 }
 
-static void __exit cleanup_srandom (void)
+static void __exit cleanup_srandom(void)
 {
 	printk("In cleanup\n");
 	unregister_chrdev (SRANDOM_MAJOR, "srandom");
